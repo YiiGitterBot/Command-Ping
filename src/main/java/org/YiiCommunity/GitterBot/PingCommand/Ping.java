8096fafe8aeb0e9f03e78311f0a78d5cdc60aa33
Alex Bond <alex.bondarevskiy@gmail.com>
2015-10-31 21:48:17 -0700

    BETA

---
+++ src/main/java/org/YiiCommunity/GitterBot/PingCommand/Ping.java
@@ -0,0 +1,21 @@
+package org.YiiCommunity.GitterBot.PingCommand;
+
+import com.amatkivskiy.gitter.rx.sdk.model.response.message.MessageResponse;
+import org.YiiCommunity.GitterBot.api.Command;
+import org.YiiCommunity.GitterBot.containers.Gitter;
+
+/**
+ * Created by Alex on 10/20/15.
+ */
+public class Ping extends Command {
+    @Override
+    public void onMessage(MessageResponse message) {
+        if (message.text.equals("ping")) {
+            try {
+                Gitter.sendMessage("pong");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
